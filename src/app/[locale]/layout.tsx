3f4527747480fc5ae7d7ac8fb4a9c6ce857b1bee
Farkas Bence <165921343+benyo22@users.noreply.github.com>
2026-01-16 17:59:38 +0100

    add vercel speed-insights

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import { getTranslations } from 'next-intl/server';
 
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Toaster } from 'sonner';
 
 import { Providers } from '@/src/components/providers/providers';
@@ -57,6 +58,7 @@ export default async function RootLayout({
       data-scroll-behavior="smooth"
     >
       <body className="bg-background">
+        <SpeedInsights />
         <Toaster />
         <Providers>{children}</Providers>
       </body>
